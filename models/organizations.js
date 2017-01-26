e386e73dad0e1dd477d51c406c5f42cb717307f7
Mircea Onciulescu <mircea.onciulescu@gmail.com>
2017-01-25 19:09:08 -0500

    add publink model

--- models/organizations.js
+++ models/organizations.js
@@ -1,14 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var passportLocalMongoose = require('passport-local-mongoose');
 
 
 var Organizations = new Schema({
     organizationName: { type: String, required: true },
     contactName: String,
     contactEmail: String,
-    contactPhone :String
-	,
+    contactPhone :String,
+    isPublisher: String,
 	entities : [{type: mongoose.Schema.Types.ObjectId, 
         ref: 'Entities' }]
 });
---
+++ publinks.js
@@ -0,0 +1,28 @@
+// JavaScript source code
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Publinks = new Schema({
+    name: { type: String },
+    link: { type: String, required: true },
+    images: [{
+        img: {
+            data: Buffer,
+            imgLink: String,
+            contentType: String,
+            description: String,
+            title: String,
+            alt: String
+        }
+    }],
+    description: String,
+    longDescription: String,
+    providerRefNo: String,
+    publisherRef: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Publisher' }],
+    isActive: Boolean,
+    startDate: Date,
+    endDate: Date
+});
+
+
+module.exports = mongoose.model('Publinks', Publinks);
